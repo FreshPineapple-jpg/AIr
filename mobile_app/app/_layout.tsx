0854d2aba767f42ca55a54e975bc8183e177ee93
JNikolo <125705821+JNikolo@users.noreply.github.com>
2024-11-08 18:05:14 -0500

    feat: Add tamagui for styling

--- mobile_app/app/_layout.tsx
+++ mobile_app/app/_layout.tsx
@@ -1,3 +1,7 @@
+import '../tamagui-web.css'
+import { TamaguiProvider } from 'tamagui'
+import { tamaguiConfig } from '../tamagui.config'
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -27,11 +31,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
